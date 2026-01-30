9754937c09eab2c5edcc9fde06f6310503e5f8bd
Sara <saramagalhaes242@gmail.com>
2026-01-30 15:31:06 -0300

    violando o NSR

--- O_Programador_Pragmático/duplicacao_inadvertida.cpp
+++ O_Programador_Pragmático/duplicacao_inadvertida.cpp
@@ -1,6 +1,19 @@
 class Line {
-public:
-    Point start;
-    Point end;
-    double length() {return start.distanceTo(end)};
+    private:
+        bool changed;
+        double length;
+        Point start;
+        Point end;
+    public:
+        void setStart(Point p) {start = p; changed = true; }
+        void setEnd(Point p) {end = p; changed = true; }
+        Point getStart(void) {return start; }
+        Point getEnd(void) {return end;}
+        double getLength(){
+            if (changed) {
+                length = start.distance(end);
+                changed = false;
+            }
+            return length;
+        }
 };
